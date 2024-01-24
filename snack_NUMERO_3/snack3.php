9f4969283f370142ffe99f4c4c14cc13f88465fc
DorinVieru <dorino.vieru@gmail.com>
2024-01-24 19:00:36 +0100

    [UP] Complete snack number 3 & end exercise

--- snack_NUMERO_3/snack3.php
+++ snack_NUMERO_3/snack3.php
@@ -1,5 +1,57 @@
 <!-- SEZIONE PHP CON LA LOGICA -->
-
+<?php
+// ARRAY DI POST
+$posts = [
+    '01-05-2012' => [
+        [
+            'title' => 'Post Attento',
+            'author' => 'Silvio Tralenuvole',
+            'text' => 'Una volta, tanto tempo fa.. non mi ricordo altro!!',
+        ],
+        [
+            'title' => 'Post Controverso',
+            'author' => 'Anna Precisa',
+            'text' => 'Secondo me è colpa di Silvio, comunque sono stato io, ma non quel giorno, ma ora purtroppo si.',
+        ],
+    ],
+    '01-08-2012' => [
+        [
+            'title' => 'Post Inventato',
+            'author' => 'Lorenzo Perluso',
+            'text' => 'Ho perso le idee, scrivo questo.',
+        ],
+        [
+            'title' => 'Post Male',
+            'author' => 'Alfredo Ginnoni',
+            'text' => 'Vorrei scrivere, ma mi hanno rubato la penna e la mia anima.',
+        ],
+    ],
+    '01-01-2013' => [
+        [
+            'title' => 'Post Scritto',
+            'author' => 'Sara Ancora',
+            'text' => 'Non metto volutamente gli apostrofi in questo testo, perché altrimenti dovrei mettere i doppi apicetti',
+        ],
+        [
+            'title' => 'Post Bene',
+            'author' => 'Stefano Malvagio',
+            'text' => 'Non sono molto furbo ma non so cosa scrivere.',
+        ],
+    ],
+    '01-09-2019' => [
+        [
+            'title' => 'Post Senza Voglia',
+            'author' => 'Roberto Incrusori',
+            'text' => 'Forse in un post manca il punto, ma non ho voglia di correggerlo.',
+        ],
+        [
+            'title' => 'Post Senza Un Titolo Decente',
+            'author' => 'Sandro Alladeriva',
+            'text' => 'Incredibile sono arrivato a questo ultimo post, e chi lo avrebbe mai detto!',
+        ],
+    ],
+]
+?>
 
 <!-- SEZIONE HTML -->
 <!DOCTYPE html>
@@ -14,7 +66,28 @@
 </head>
 
 <body>
+    <div class="container d-flex justify-content-center my-5">
+        <div class="row w-75">
+            <div class="col-12">
+                <h1 class="text-center text-success pb-4">I post più belli che leggerai in tuta divisi per data</h1>
+                <ul class="list-group text-center">
+                    <?php foreach ($posts as $date => $postsInDate) { ?>
+                        <?php echo "<h2 class='text-info pt-3'>Data: $date</h2>" ?>
 
+                        <?php foreach ($postsInDate as $post) { ?>
+                            <li class="list-group-item">
+                                <?php
+                                echo "<h3>{$post['title']}</h3>";
+                                echo "<p><span class='fw-bold text-warning'>Autore:</span> {$post['author']}</p>";
+                                echo "<p>{$post['text']}</p>";
+                                ?>
+                            </li>
+                        <?php } ?>
+                    <?php } ?>
+                </ul>
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
